eb1a6b86e75848098dc60a1090e5914b12189716
ZarredFelicite <zarred.f@gmail.com>
2025-06-03 08:12:25 +1000

    add mail flag to flake script

--- flake.nix
+++ flake.nix
@@ -338,7 +338,7 @@
     devShells.default = pkgs.mkShell {
       name = "yolo";
       buildInputs = systemPackages ++ [
-        ( pkgs.writeScriptBin "pipes" "./pipesight.py -c 0.15 -svp" )
+        ( pkgs.writeScriptBin "pipes" "./pipesight.py -c 0.15 -svpm" )
         python
         python.pkgs.setuptools
         python.pkgs.requests
